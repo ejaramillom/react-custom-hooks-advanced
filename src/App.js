c9863f878fa9820c0f7ecc403a1d0c40b32f3758
Emmanuel <ejaramilo@hotmail.com>
2022-10-14 08:31:09 -0500

    192 creating custom hook for multiple request types

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  useHttp({
+    url: 'https://react-http-59b0a-default-rtdb.firebaseio.com/tasks.json'
+  });
 
   const fetchTasks = async (taskText) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-http-59b0a-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
